4bbe276cfd8bb8684f0ea9f7e207203db4ada744
AbedAmina <124576452+AbedAmina@users.noreply.github.com>
2023-04-17 13:54:28 +0000

    Updatepalindrom.cs

--- Palindrom1.cs
+++ Palindrom1.cs
@@ -1,15 +1,4 @@
-bool Solution (string inputString)
+bool solution(string myString) 
 {
-    int i = 0;
-    int j = inputString.Length - 1;
-    while (i < j)
-    {
-        if (inputString[i] != inputString[j])
-        {
-            return false;
-        }
-        i++;
-        j--;
-    }
-    return true;
+    return myString.SequenceEqual(myString.Reverse());
 }
